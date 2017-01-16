234a9eadde359be23ad41036d579be037235a34f
Saiful Bashar <robal.bashar59@gmail.com>
2017-01-16 19:12:37 +0600

    play with middleware

--- admin.js
+++ admin.js
@@ -8,34 +8,31 @@ var _ = require("lodash"),
 var router = express.Router();
 module.exports = router;
 
-
 router.get("/rooms", function (req, res) {
     res.render('room.jade', {
         title: "Admin Rooms",
         rooms: rooms
     });
 });
-
-
 //route to /room/add  where add chat room
-router.get("/rooms/add", function (req, res) {
-    res.render('add.jade');
-});
 
+router.route("/rooms/add")
+    .get(function (req, res) {
+        res.render('add.jade');
+    })
+    //add chatroom to rooms.json
+    //redirect to /rooms
+    //bodyParser is used to get POST value
+    .post(function (req, res) {
+        var room = {
+            name: req.body.name,
+            id: uuid.v4()
+        };
 
-//add chatroom to rooms.json
-//redirect to /rooms
-//bodyParser is used to get POST value
-router.post('/rooms/add', function (req, res) {
-    var room = {
-        name: req.body.name,
-        id: uuid.v4()
-    };
+        rooms.push(room);
 
-    rooms.push(room);
-
-    res.redirect(req.baseUrl+"/rooms");
-});
+        res.redirect(req.baseUrl + "/rooms");
+    });
 
 //delete chat room
 //get uuid from url
@@ -43,27 +40,26 @@ router.post('/rooms/add', function (req, res) {
 router.get('/rooms/delete/:id', function (req, res) {
     var roomId = req.params.id;
     rooms = rooms.filter(p => p.id != roomId);
-    res.redirect(req.baseUrl+"/rooms");
+    res.redirect(req.baseUrl + "/rooms");
 
 });
 
 //edit chatroom
-router.get('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
-    res.render("edit.jade", {room});
-});
-router.post('/rooms/edit/:id', function (req, res) {
-    var roomId = req.params.id;
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
-    room.name = req.body.name;
-    res.redirect(req.baseUrl+"/rooms");
-});
+router.route('/rooms/edit/:id')
+    .all(function (req,res,next) {
+        var roomId = req.params.id;
+        var room = _.find(rooms, r => r.id === roomId);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
+        res.locals.room = room;
+        next();
+    })
+    .get(function (req, res) {
+        res.render("edit.jade");
+    })
+    .post(function (req, res) {
+        res.locals.room.name = req.body.name;
+        res.redirect(req.baseUrl + "/rooms");
+    });
