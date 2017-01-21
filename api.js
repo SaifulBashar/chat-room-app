69ca802fa49794a29c2713410b92e153b6ee18af
Saiful Bashar <robal.bashar59@gmail.com>
2017-01-21 11:27:46 +0600

    post message and delete by specific room id

--- api.js
+++ api.js
@@ -11,18 +11,37 @@ router.get("/rooms", function (req, res) {
     res.json(rooms);
 });
 
-router.get("/rooms/:roomid/messages",function (req,res) {
-    var roomId = req.params.roomid;
-    var roomMessage = messages.filter(m => m.roomId === roomId);
-    var room = _.find(rooms, r => r.id === roomId);
-    console.log(room);
-    if(!room){
-        res.sendStatus(404);
-        return;
-    }
+router.route("/rooms/:roomid/messages")
+    .get(function (req, res) {
+        var roomId = req.params.roomid;
+        var roomMessage = messages.filter(m => m.roomId === roomId);
+        var room = _.find(rooms, r => r.id === roomId);
+        console.log(room);
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
 
-    res.json({
-        room:room,
-        messages:roomMessage
-    });
-});
+        res.json({
+            room: room,
+            messages: roomMessage
+        });
+    })
+    .post(function (req, res) {
+        var roomId = req.params.roomid;
+        var message = {
+            roomId: roomId,
+            text: req.body.text,
+            userId: "44f885e8-87e9-4911-973c-4074188f408a",
+            id :uuid.v4()
+        };
+        messages.push(message);
+        //res.sendStatus(200);
+        res.json(messages);
+    })
+    .delete(function (req,res) {
+        var roomId = req.params.roomid;
+        messages = messages.filter(m => m.roomId !==roomId);
+        res.sendStatus(200);
+    })
+
